210a42baf7307255e801c233cf61808dd1dd54c5
hasan-droid <hasan-net2010@hotmail.com>
2021-07-10 01:51:07 +0300

    add-book route , remove-book route

--- Model/books.model.js
+++ Model/books.model.js
@@ -1,12 +1,16 @@
 'use-strict'
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-const bookSchema=mongoose.Schema({
-    name:{type:String},
-    description:{type:String},
-    status:{type:String}
+const bookSchema = mongoose.Schema({
+    name: { type: String },
+    description: { type: String },
+    status: { type: String }
 })
+
+
 
 //const bookModel=mongoose.Model('bookSchema' , bookSchema)
 
-module.exports=bookSchema;
+module.exports = bookSchema;
+
+
--- server.js
+++ server.js
@@ -1,49 +1,57 @@
 'use strict';
 
 require('dotenv').config();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/favBooks', { useNewUrlParser: true, useUnifiedTopology: true });
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa');
-const database=require('./DataBase')
-const booksroute=require('./Controller/books.controller');
-const bookscontroller = require('./Controller/books.controller');
+const database = require('./DataBase')
 
-
-
+// const {booksroute,
+//   createbook
+// }=require('./Controller/books.controller');
+const { bookscontroller,
+  createbook,
+  deleteBook } = require('./Controller/books.controller');
 const app = express();
 app.use(cors());
+app.use(express.json());
+
 
 const PORT = process.env.PORT || 3001;
 
 app.get('/test', (request, response) => {
-response.send('test'); 
+  response.send('test');
 
 })
 const client = jwksClient({
   jwksUri: `https://${process.env.AUTH0_DOMAIN_NAME}/.well-known/jwks.json`
 });
 
-const getKey=(header, callback)=>{
-  client.getSigningKey(header.kid, function(err,key){
-    const signingKey= key.publicKey || key.rsaPublicKey; 
+const getKey = (header, callback) => {
+  client.getSigningKey(header.kid, function (err, key) {
+    const signingKey = key.publicKey || key.rsaPublicKey;
     callback(null, signingKey)
-  }); 
+  });
 }
 
-app.get('/authorize', (req,res)=>{
-  const token= req.headers.authorization.split(' ')[1]; 
-  jwt.verify(token,getKey,{},(err,user)=>{
-    if (err){
-      res.send('invalid token'); 
+app.get('/authorize', (req, res) => {
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, getKey, {}, (err, user) => {
+    if (err) {
+      res.send('invalid token');
     }
-  //  res.send(user)
+    //  res.send(user)
   })
- // res.send( token);
+  res.send(token);
 })
 
 app.get('/database', database)
-app.get('/books',bookscontroller)
+app.get('/books', bookscontroller)
+app.post('/add-book', createbook);
+app.delete('/removebook/:index', deleteBook)
 
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
